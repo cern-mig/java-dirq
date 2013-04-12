082d607aa1591b039fa18560573a0ae127fd2330
Lionel Cons <lionel.cons@cern.ch>
2013-04-12 13:03:03 +0200

    use error names instead of codes

--- src/main/java/ch/cern/mig/posix/BasePosix.java
+++ src/main/java/ch/cern/mig/posix/BasePosix.java
@@ -7,28 +7,28 @@ import com.sun.jna.LastErrorException;
 import com.sun.jna.Platform;
 
 public class BasePosix {
-    public static final int EPERM = 1;
-    public static final int ENOENT = 2;
-    public static final int ESRCH = 3;
-    public static final int EINTR = 4;
-    public static final int EIO = 5;
-    public static final int ENXIO = 6;
-    public static final int E2BIG = 7;
-    public static final int ENOEXEC = 8;
-    public static final int EBADF = 9;
-    public static final int ECHILD = 10;
-    public static final int EAGAIN = 11;
-    public static final int ENOMEM = 12;
-    public static final int EACCES = 13;
-    public static final int EFAULT = 14;
+    public static final int EPERM   =  1;
+    public static final int ENOENT  =  2;
+    public static final int ESRCH   =  3;
+    public static final int EINTR   =  4;
+    public static final int EIO     =  5;
+    public static final int ENXIO   =  6;
+    public static final int E2BIG   =  7;
+    public static final int ENOEXEC =  8;
+    public static final int EBADF   =  9;
+    public static final int ECHILD  = 10;
+    public static final int EAGAIN  = 11;
+    public static final int ENOMEM  = 12;
+    public static final int EACCES  = 13;
+    public static final int EFAULT  = 14;
     public static final int ENOTBLK = 15;
-    public static final int EBUSY = 16;
-    public static final int EEXIST = 17;
-    public static final int EXDEV = 18;
-    public static final int ENODEV = 19;
+    public static final int EBUSY   = 16;
+    public static final int EEXIST  = 17;
+    public static final int EXDEV   = 18;
+    public static final int ENODEV  = 19;
     public static final int ENOTDIR = 20;
-    public static final int EISDIR = 21;
-    public static final int EINVAL = 22;
+    public static final int EISDIR  = 21;
+    public static final int EINVAL  = 22;
 
     public LibC libc;
 
@@ -69,7 +69,7 @@ public class BasePosix {
     public File opendir(String path) throws LastErrorException {
         File dir = new File(path);
         if (!dir.exists())
-            throw new LastErrorException(2);
+            throw new LastErrorException(ENOENT);
         return dir;
     }
 
@@ -80,16 +80,16 @@ public class BasePosix {
             result = file.createNewFile();
         } catch (IOException e) {
             if (e.getMessage().equals("No such file or directory"))
-                throw new LastErrorException(2);
+                throw new LastErrorException(ENOENT);
             throw new LastErrorException(e.getMessage());
         }
         if (result)
             if (file.canWrite())
                 return file;
             else
-                throw new LastErrorException(13);
+                throw new LastErrorException(EACCES);
         else
-            throw new LastErrorException(17);
+            throw new LastErrorException(EEXIST);
     }
 
     public void rename(String from, String to) throws LastErrorException {
