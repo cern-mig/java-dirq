f8e9607a64a0059ba39601d29b3179a111122c0d
Lionel Cons <lionel.cons@cern.ch>
2020-02-03 11:05:04 +0100

    refreshed the dependencies

--- src/test/java/ch/cern/dirq/FileUtilsTest.java
+++ src/test/java/ch/cern/dirq/FileUtilsTest.java
@@ -16,7 +16,7 @@ import org.junit.rules.TemporaryFolder;
  *
  * @author Lionel Cons &lt;lionel.cons@cern.ch&gt;
  * @author Massimo Paladin &lt;massimo.paladin@gmail.com&gt;
- * Copyright (C) CERN 2012-2019
+ * Copyright (C) CERN 2012-2020
  */
 public class FileUtilsTest {
 
