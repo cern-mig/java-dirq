3bce5120eeddb2f8d5f4f692c81a2f0b57a9d146
Lionel Cons <lionel.cons@cern.ch>
2021-08-03 12:16:30 +0200

    updated the copyright dates

--- src/main/java/ch/cern/dirq/FileUtils.java
+++ src/main/java/ch/cern/dirq/FileUtils.java
@@ -21,7 +21,7 @@ import java.util.Set;
  *
  * @author Lionel Cons &lt;lionel.cons@cern.ch&gt;
  * @author Massimo Paladin &lt;massimo.paladin@gmail.com&gt;
- * Copyright (C) CERN 2012-2020
+ * Copyright (C) CERN 2012-2021
  */
 public final class FileUtils {
 
--- src/main/java/ch/cern/dirq/package-info.java
+++ src/main/java/ch/cern/dirq/package-info.java
@@ -6,6 +6,6 @@
  *
  * @author Lionel Cons &lt;lionel.cons@cern.ch&gt;
  * @author Massimo Paladin &lt;massimo.paladin@gmail.com&gt;
- * Copyright (C) CERN 2012-2020
+ * Copyright (C) CERN 2012-2021
  */
 package ch.cern.dirq;
